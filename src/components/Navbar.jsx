45e0dce69d0464b5c1faeb055c3a98d1c91a578f
HarshaInukollu123 <harshainukollu3@gmail.com>
2025-05-12 09:33:23 -0500

    minor UX change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,32 +7,29 @@ const Navbar = () => {
   const isActive = (path) =>
     location.pathname === path
       ? 'text-blue-600 font-semibold'
-      : 'text-gray-700 hover:text-blue-600';
+      : 'text-gray-700 hover:text-yellow-200 ';
 
   return (
-    <nav className="bg-white shadow-sm sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16 items-center">
-          <div className="flex-shrink-0 text-2xl font-bold text-blue-700">
-            InventoryApp
-          </div>
-          <div className="flex space-x-6">
-            <Link to="/" className={`text-sm ${isActive('/')}`}>
-              Dashboard
-            </Link>
-            <Link to="/products" className={`text-sm ${isActive('/products')}`}>
-              Products
-            </Link>
-            <Link to="/analytics" className={`text-sm ${isActive('/analytics')}`}>
-              Analytics
-            </Link>
-            {/* <Link to="/sales" className={`text-sm ${isActive('/sales')}`}>
-              Sales
-            </Link> */}
-          </div>
+    <nav className="bg-indigo-600 shadow text-white z-50 relative">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="flex justify-between h-16 items-center">
+        <div className="flex-shrink-0 text-2xl font-bold text-white">
+          InventoryApp
+        </div>
+        <div className="flex space-x-6">
+          <Link to="/" className={`text-sm text-white ${isActive('/')}`}>
+            Dashboard
+          </Link>
+          <Link to="/products" className={`text-sm text-white ${isActive('/products')}`}>
+            Products
+          </Link>
+          <Link to="/analytics" className={`text-sm text-white ${isActive('/analytics')}`}>
+            Analytics
+          </Link>
         </div>
       </div>
-    </nav>
+    </div>
+  </nav>
   );
 };
 
