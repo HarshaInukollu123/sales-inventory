49549209f0ecbd0e47fce3137a650912041264d1
HarshaInukollu123 <harshainukollu3@gmail.com>
2025-05-12 10:09:20 -0500

    minor fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,8 +14,9 @@ const Navbar = () => {
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex justify-between h-16 items-center">
         <div className="flex-shrink-0 text-2xl font-bold text-white">
-          InventoryApp
-        </div>
+        <Link to="/" className={`text-white ${isActive('/')}`}>
+            Inventory App
+          </Link>        </div>
         <div className="flex space-x-6">
           <Link to="/" className={`text-sm text-white ${isActive('/')}`}>
             Dashboard
